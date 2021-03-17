9deb4a54de2ac28434d6e1ffa6347345e6cce2cf
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-17 15:17:40 -0700

    write #initialize, #[], and #[]= in Flash

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,23 @@
 require 'json'
 
 class Flash
+  attr_reader :flash
+  def initialize(req)
+    if req.cookies["_rails_lite_app_flash"]
+      @flash = JSON.parse(req.cookies['_rails_lite_app_flash'])
+    else
+      @flash = {}
+    end
+  end
+
+  def [](key)
+    @flash[key]
+  end
+
+  def []=(key, value)
+    @flash[key] = value
+  end
+
+  def store_flash(res)
+  end
 end
