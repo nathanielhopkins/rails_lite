02af551a065498c482025313b7d0e343a616d101
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-19 15:11:06 -0700

    create ivar @res in Static#initialize; begin writing Static#call; write private method Static#root_path to help route to public folder from file; add attr_reader for :app, :res, and :req.

--- lib/static.rb
+++ lib/static.rb
@@ -1,8 +1,28 @@
+require 'rack'
+
 class Static
+  attr_reader :app, :req, :res
+  
   def initialize(app)
     @app = app
+    @res = Rack::Response.new
   end
 
   def call(env)
+    dir_path = root_path
+    @res["Content-Type"] = env[1]["Content-Type"]
+    
+    @req = Rack::Request.new(env)
+    #somehow find file from request and set to variable 'content'
+    body = File.read(content)
+    @res.write(body)
+    @res.finish
+  end
+
+  private
+  def root_path
+    dir_name = File.dirname(__FILE__)
+    root = File.dirname(dir_name)
+    dir_path = File.join(root, "public")
   end
 end
