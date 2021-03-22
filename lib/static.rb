4df2189387ea97df24b37e36b2ae0c68d0abdd29
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-03-22 13:05:29 -0700

    move logic for serving files from Static#call to Static#serve; if path in env includes /public/, #call will call Static#serve, else call @app.call; create method #mime_type and ivar @MIME_TYPES to write Content-Type into response from included extenstion in the path

--- lib/static.rb
+++ lib/static.rb
@@ -6,24 +6,46 @@ class Static
   def initialize(app)
     @app = app
     @res = Rack::Response.new
+    @MIME_TYPES = {
+    '.txt' => 'text/plain',
+    '.jpg' => 'image/jpeg',
+    '.zip' => 'application/zip'
+    }
   end
 
   def call(env)
+    if env["PATH_INFO"].index("/public/")  
+      serve(env)
+    else
+      @app.call(env)
+    end
+  end
+
+  
+  private
+  def serve(env)
     content = file_path(env)
     if File.exist?(content)
       body = File.read(content)
       @res.write(body)
+      mime_type(content)
     else
       @res.status = 404
+      @res.write("File not found")
     end
     @res.finish
   end
-  
-  private
+
   def file_path(env)
     dir_name = File.dirname(__FILE__)
     root = File.dirname(dir_name)
     file_path = env["PATH_INFO"]
     content = File.join(root, file_path)
+  end
+
+  def mime_type(file_name)
+    ext = File.extname(file_name)
+    content_type = @MIME_TYPES[ext]
+    @res["Content-Type"] = content_type
   end
 end
